ba5b7838fb9b080f68cc8518452d6be84de66f18
jayden <chongjayden07@gmail.com>
2024-09-16 11:14:19 +0800

    back to home button in game pages

--- src/app/(games)/games/page.tsx
+++ src/app/(games)/games/page.tsx
@@ -18,6 +18,7 @@ export default async function GameHomePage() {
 		<div className={`h-svh p-8 flex flex-col`} style={{backgroundColor: config.GameTheme.background}}>
 			<h1 className="font-bold text-2xl text-white pb-4">Select new game</h1>
 			<ScenarioSelectionWindow scenarioList={scenarioList} />
+			<a href="/" className="font-bold text-white underline">Go to home</a>
 			<Alerts message={errorMessage} />
 		</div>
 	)
