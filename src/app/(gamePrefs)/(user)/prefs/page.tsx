929fcf2e304dbe81a979d129af32d23bb0fff6ca
ballgoesvroomvroom <chongjayden07@gmail.com>
2024-09-20 20:36:44 +0800

    show email on `/prefs`

--- src/app/(gamePrefs)/(user)/prefs/page.tsx
+++ src/app/(gamePrefs)/(user)/prefs/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect, useContext, useState } from "react"
+import { ProfileDataContext } from "@/app/lib/ui/contextWrapper"
 import { GamePrefContext } from "@/app/(gamePrefs)/gamePrefsContext"
 import { langList, langDict } from "@/app/lib/languages"
 
@@ -8,14 +9,15 @@ import { langList, langDict } from "@/app/lib/languages"
 export default function Prefs() {
 	// change prefs
 	const { prefs, setPrefs } = useContext(GamePrefContext)
+	const { profileData } = useContext(ProfileDataContext)
 
 	return (
 		<div className="grow flex flex-col gap-2 p-8">
 			<div className="grow flex flex-col gap-10">
 				<section className="flex flex-col gap-2">
-					<h2 className="mb-1 font-bold text-lg">Game preferences</h2>
+					<h2 className="mb-1 font-bold text-xl">Game preferences</h2>
 					<label className="font-bold">Language</label>
-					<select className="p-2" value={prefs.lang} onChange={e => {
+					<select className="p-2 rounded bg-white" value={prefs.lang} onChange={e => {
 						prefs.lang = parseInt(e.target.value) as 0|1|2|3
 						console.log("setPrefs", setPrefs)
 						if (setPrefs) {
@@ -30,7 +32,10 @@ export default function Prefs() {
 					</select>
 				</section>
 				<section className="flex flex-col gap-2">
-					<h2 className="mb-1 font-bold text-lg">Account</h2>
+					<h2 className="mb-1 font-bold text-xl">Account</h2>
+					<label className="font-bold">Email</label>
+					<input className="p-2 rounded bg-white" type="text" value={profileData?.email} disabled />
+					<p className="text-sm mb-4">Please reach out to support@brainbloom.ai to change email address.</p>
 					<a href="/logout" className="p-2 rounded bg-black text-white font-bold">Logout</a>
 				</section>
 			</div>
