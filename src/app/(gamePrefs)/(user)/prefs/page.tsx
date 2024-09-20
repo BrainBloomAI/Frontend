74032604692d841c806075a1aaacfccf046f7142
ballgoesvroomvroom <chongjayden07@gmail.com>
2024-09-20 19:26:07 +0800

    added logout button

--- src/app/(gamePrefs)/(user)/prefs/page.tsx
+++ src/app/(gamePrefs)/(user)/prefs/page.tsx
@@ -10,20 +10,27 @@ export default function Prefs() {
 
 	return (
 		<div className="grow flex flex-col gap-2 p-8">
-			<div className="grow flex flex-col gap-2">
-				<label className="font-bold">Language</label>
-				<select className="p-2" value={prefs.lang} onChange={e => {
-					prefs.lang = parseInt(e.target.value) as 0|1|2|3
-					console.log("setPrefs", setPrefs)
-					if (setPrefs) {
-						setPrefs(Object.assign({}, prefs))
-					}
-				}}>
-					<option value="0">English</option>
-					<option value="1">Mandarin</option>
-					<option value="2">Behasa Melayu</option>
-					<option value="3">Hindu</option>
-				</select>
+			<div className="grow flex flex-col gap-10">
+				<section className="flex flex-col gap-2">
+					<h2 className="mb-1 font-bold text-lg">Game preferences</h2>
+					<label className="font-bold">Language</label>
+					<select className="p-2" value={prefs.lang} onChange={e => {
+						prefs.lang = parseInt(e.target.value) as 0|1|2|3
+						console.log("setPrefs", setPrefs)
+						if (setPrefs) {
+							setPrefs(Object.assign({}, prefs))
+						}
+					}}>
+						<option value="0">English</option>
+						<option value="1">Mandarin</option>
+						<option value="2">Behasa Melayu</option>
+						<option value="3">Hindu</option>
+					</select>
+				</section>
+				<section className="flex flex-col gap-2">
+					<h2 className="mb-1 font-bold text-lg">Account</h2>
+					<a href="/logout" className="p-2 rounded bg-black text-white font-bold">Logout</a>
+				</section>
 			</div>
 			<a href="/profile" className="font-bold underline">Go back</a>
 		</div>
